5d07e92390e717bb75d735ffa3dcc4ad29f37cfb
Zack <zainthaver262@gmail.com>
2026-02-06 13:43:33 -0500

    changed server client error

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,9 +1,10 @@
 import { redirect } from 'next/navigation'
 
+import { createClient } from '../../../utils/supabase/server'
 import DemoClientComponent from '../components/DemoClientComponent'
 
 export default async function PrivatePage() {
-  const supabase = await createServerClient()
+  const supabase = await createClient()
 
   const { data, error } = await supabase.auth.getUser()
   if (error || !data?.user) {
