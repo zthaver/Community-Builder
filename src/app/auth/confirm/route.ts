24c5ff1fa5433143ca720999eb977b3c259a7b54
Zack <zainthaver262@gmail.com>
2026-02-06 14:15:51 -0500

    built nextjs app

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,7 +1,7 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest } from 'next/server'
 
-import { createServerClient } from '../../../../utils/supabase/server'
+import { createClient } from '../../../../utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 export async function GET(request: NextRequest) {
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
   const next = searchParams.get('next') ?? '/'
 
   if (token_hash && type) {
-    const supabase = await createServerClient();
+    const supabase = await createClient();
 
     const { error } = await supabase.auth.verifyOtp({
       type,
